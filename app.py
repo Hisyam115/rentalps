956f9073542bd8a1e008561196ab82412c35a7c6
sammm <156216443+Hisyam115@users.noreply.github.com>
2024-12-26 09:16:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 
 # Konfigurasi database
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",  # Ganti dengan username MySQL Anda
-    password="",  # Ganti dengan password MySQL Anda
-    database="fprentalps"  # Ganti dengan nama database Anda
+    user="rentalps", 
+    password="Adminrental123", 
+    host="rentalps2.mysql.database.azure.com", 
+    port=3306, 
+    database="fprentalps"
 )
 
 # Secret key for session management
